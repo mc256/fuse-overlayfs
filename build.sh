d6d838389f236bc88098e062b7b00a569f09666f
Maverick Chan <ios@mc256.com>
2020-07-19 23:32:25 -0400

    Fast build with local cache

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-buildah bud -v $PWD:/build/fuse-overlayfs -t fuse-overlayfs -f ./Dockerfile.static.ubuntu.remote .
+buildah bud -v $PWD:/build/fuse-overlayfs -t fuse-overlayfs -f ./Dockerfile.static.ubuntu.remote.fast .
 cp ./fuse-overlayfs ../../test/fuse-overlayfs
 chown maverick:maverick ../../test/fuse-overlayfs
 chmod 777 ../../test/fuse-overlayfs
